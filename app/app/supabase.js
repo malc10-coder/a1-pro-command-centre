0651468324671bc28be1f03dd284ccf30883e8c6
malc10-coder <malc10@hotmail.co.uk>
2026-08-31 15:28:02 +0100

    Create supabase.js

---
+++ app/app/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'YOUR_SUPABASE_URL';
+
+const supabaseKey = 'YOUR_PUBLISHABLE_KEY';
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
