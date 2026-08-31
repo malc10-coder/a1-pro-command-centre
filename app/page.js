cb5153875d0cd6687076d57ac004253919a8694f
malc10-coder <malc10@hotmail.co.uk>
2026-08-31 16:10:12 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
-import { useMemo, useState } from 'react';
-import {
+import { useEffect,useMemo, useState } from 'react';
+import {supabase ) from '../supabase';
   LayoutDashboard,
   Briefcase,
   CalendarDays,
